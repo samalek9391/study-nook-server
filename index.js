b8d2926f7b2a6b01754dddd09a0311a5db24e9ac
samalek9391 <sa.malek9391@gmail.com>
2026-06-15 22:05:30 +0600

    fix some issues

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require('express');
 const dotenv = require('dotenv')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 dotenv.config()
